c6fd2b1cdacf98ca69cb01bdcc0534bb87925647
Juan Carlos Minango Negrete <42250853+jminango20@users.noreply.github.com>
2021-11-14 20:30:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ st.write(x, 'squared is', x * x)
 
 option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
 st.write('You selected:', option)
+
+st.text('Hola')
